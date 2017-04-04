b33b15cabe096c6cf32d5e02c3df433544ef54a8
Leslie A. Lopez <lesashley56@gmail.com>
2017-04-04 18:19:06 -0500

    Primer commit

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+function Coder() {
+  this.coders = [];
+  this.addCoders = function () {
+    this.coders.push({
+      name : name,
+      publication : publication
+    });
+  }
+  this.createPost= function (text,id) {
+    var div = document.createElement("div");
+    div.setAttribute("id")
+  }
+}
+
+var coder = new Coder();
+
+
+
+document.getElementById("submit").addEventListener("click",function () {
+
+});
