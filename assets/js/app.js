0ed21b5a8cfb0c13bd585fdc7ca7cfc6be2ad958
lesashley <lesashley56@gmail.com>
2017-04-05 21:56:21 -0500

    Funcion en js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,43 @@
+var documentFragment = document.createDocumentFragment();
 function Coder() {
   this.coders = [];
-  this.addCoders = function () {
+  this.addCoders = function (autor,text) {
     this.coders.push({
-      name : name,
-      publication : publication
+      name : autor,
+      publication : text
     });
   }
-  this.createPost= function (text,id) {
-    var div = document.createElement("div");
-    div.setAttribute("id")
+  this.createPost= function (autor,text) {
+    var content = document.createElement("article");
+    content.setAttribute("class","content-text");
+    var status = document.createElement("div");
+    status.setAttribute("class","estado");
+    status.textContent = text;
+    var name = document.createElement("div");
+    name.setAttribute("class", "name");
+    name.textContent=autor;
+    var deleteContent = document.createElement("button");
+    deleteContent.setAttribute("class", "button");
+    deleteContent.textContent="x";
+    deleteContent.addEventListener("click",function () {
+      content.removeChild(status);
+      content.removeChild(name);
+      content.removeChild(deleteContent);
+      content.parentNode.removeChild(content);
+    });
+    content.appendChild(status);
+    content.appendChild(name);
+    content.appendChild(deleteContent);
+    documentFragment.appendChild(content);
+    document.getElementById("status").prepend(documentFragment);
+    this.addCoders(autor,text);
+    }
   }
-}
 
 var coder = new Coder();
 
-
-
 document.getElementById("submit").addEventListener("click",function () {
-
+  var author = document.getElementById("nickname").value;
+  var textCoder = document.getElementById("text-coder").value;
+  coder.createPost(author,textCoder);
 });
